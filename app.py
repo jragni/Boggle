96d4af892338b23ac6f741fe9589ac14bc0d24fe
jragni <jagni001@ucr.edu>
2021-07-09 16:05:38 -0700

    Added score_score()

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from flask import Flask, request, render_template, jsonify, session
 from uuid import uuid4
-
+from flask_debugtoolbar import DebugToolbarExtension
 from boggle import BoggleGame
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "this-is-secret"
-
+debug = DebugToolbarExtension(app)
 # The boggle games created, keyed by game id
-games = {}
-
+games = {
+    1 : BoggleGame() # NOTE: used for testing
+}
 
 @app.route("/")
 def homepage():
     """Show board."""
-
+    
     return render_template("index.html")
 
 
@@ -24,8 +25,33 @@ def new_game():
     game_id = str(uuid4())
     game = BoggleGame()
     games[game_id] = game
-
-
     return jsonify( {"gameId":game_id, "board":game.board} )
 
 
+@app.route('/api/score-word', methods=["POST"])
+def score_word():
+    """Scores word submitted in form"""    
+    #It should be in the word list
+    #It sound be findable on the board
+
+    new_word = request.form('word')
+
+    curr_game_id = 1  # NOTE: used for testing 
+
+    current_game = games[curr_game_id]   # NOTE: used for testing 
+    
+    if current_game.is_word_in_word_list(new_word):
+        pass
+    elif current_game.is_word_not_a_dup(new_word):
+        pass
+    elif current_game.check_word_on_board(new_word):
+        current_game.play_and_score_word(new_word)
+    
+    #update game in games
+
+    games[curr_game_id] = current_game
+
+    
+
+    
+    
